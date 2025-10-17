8cc5b58029a4ebf2febe5aa10ca6cdbde92f82ce
angelalita77 <anjeanette@gmail.com>
2025-10-17 15:15:48 -0400

    Completed a simple fetch of the data from YouTube API Data v3

---
+++ notes/yt_api_nujabes.js
@@ -0,0 +1,68 @@
+{
+    "kind": "youtube#searchListResponse",
+    "etag": "dQ2M9QcAKg9g8tnCDyazplGiETM",
+    "nextPageToken": "CAIQAA",
+    "regionCode": "US",
+    "pageInfo": {
+        "totalResults": 414507,
+        "resultsPerPage": 2
+    },
+    "items": [
+        {
+            "kind": "youtube#searchResult",
+            "etag": "VOLNdBF4_-AcJj_0eFqOsmPjzfI",
+            "id": {
+                "kind": "youtube#channel",
+                "channelId": "UCxQQET-tQsC4cwzLow9ZsuQ"
+            },
+            "snippet": {
+                "publishedAt": "2014-01-16T21:28:15Z",
+                "channelId": "UCxQQET-tQsC4cwzLow9ZsuQ",
+                "title": "Nujabes - Topic",
+                "description": "Musician, composer, DJ, record producer. From 1995 in Shibuya, Tokyo, Nujabes worked on his music career and in July 1995, ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://yt3.ggpht.com/52abbarzgADsxY0F8Pz7lcd0-XQaZX2Uw6pm4hlsSMwn_5Q6bxxIQOWUJABQ6RhZf7yGNtYx=s88-c-k-c0xffffffff-no-rj-mo"
+                    },
+                    "medium": {
+                        "url": "https://yt3.ggpht.com/52abbarzgADsxY0F8Pz7lcd0-XQaZX2Uw6pm4hlsSMwn_5Q6bxxIQOWUJABQ6RhZf7yGNtYx=s240-c-k-c0xffffffff-no-rj-mo"
+                    },
+                    "high": {
+                        "url": "https://yt3.ggpht.com/52abbarzgADsxY0F8Pz7lcd0-XQaZX2Uw6pm4hlsSMwn_5Q6bxxIQOWUJABQ6RhZf7yGNtYx=s800-c-k-c0xffffffff-no-rj-mo"
+                    }
+                },
+                "channelTitle": "Nujabes - Topic",
+                "liveBroadcastContent": "none",
+                "publishTime": "2014-01-16T21:28:15Z"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "yRUOjt8meiwJb33kUQqfIRn56e4",
+            "id": {
+                "kind": "youtube#channel",
+                "channelId": "UCO5XcuQe_fLJVI3sZ0LmGCA"
+            },
+            "snippet": {
+                "publishedAt": "2020-09-02T10:22:12Z",
+                "channelId": "UCO5XcuQe_fLJVI3sZ0LmGCA",
+                "title": "Nujabes",
+                "description": "Nujabes Official Channel YEN TOWN MARKET's Nujabes merchandise is an official collaborative project with Hydeout ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://yt3.ggpht.com/ytc/AIdro_kQR6ZLejMlmjEV3Jr_avZ3QucWE6b34SciJ1rGU2zXYg=s88-c-k-c0xffffffff-no-rj-mo"
+                    },
+                    "medium": {
+                        "url": "https://yt3.ggpht.com/ytc/AIdro_kQR6ZLejMlmjEV3Jr_avZ3QucWE6b34SciJ1rGU2zXYg=s240-c-k-c0xffffffff-no-rj-mo"
+                    },
+                    "high": {
+                        "url": "https://yt3.ggpht.com/ytc/AIdro_kQR6ZLejMlmjEV3Jr_avZ3QucWE6b34SciJ1rGU2zXYg=s800-c-k-c0xffffffff-no-rj-mo"
+                    }
+                },
+                "channelTitle": "Nujabes",
+                "liveBroadcastContent": "none",
+                "publishTime": "2020-09-02T10:22:12Z"
+            }
+        }
+    ]
+}
---
+++ notes/yt_api_search_with_spaces.js
@@ -0,0 +1,80 @@
+{
+    "kind": "youtube#searchListResponse",
+    "etag": "seSnhcBsM5E3sfsUusphPfQRy48",
+    "nextPageToken": "CAIQAA",
+    "regionCode": "US",
+    "pageInfo": {
+        "totalResults": 18068,
+        "resultsPerPage": 2
+    },
+    "items": [
+        {
+            "kind": "youtube#searchResult",
+            "etag": "yyAITHwJXx1-xWPdl3NRgd7_6fA",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "DHwXnEu1Fxw"
+            },
+            "snippet": {
+                "publishedAt": "2019-10-02T22:09:42Z",
+                "channelId": "UCBDtSCNOU1us3wLsekS-rbw",
+                "title": "Nujabes- Light on the Land (Homework Edit)",
+                "description": "Light on the Land, edited for your enjoyment. Album: Modal Soul (2005) Support the artist: ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/DHwXnEu1Fxw/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/DHwXnEu1Fxw/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/DHwXnEu1Fxw/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "limeyking",
+                "liveBroadcastContent": "none",
+                "publishTime": "2019-10-02T22:09:42Z"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "zKBF1uRh-TEf0YWFQkJDYQnCaq8",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "0qR3B1i3FBk"
+            },
+            "snippet": {
+                "publishedAt": "2023-01-26T08:00:00Z",
+                "channelId": "UCO5XcuQe_fLJVI3sZ0LmGCA",
+                "title": "Nujabes - Light on the land [Official Audio]",
+                "description": "Nujabes - Light on the land from Modal Soul Stream / Buy : https://opc.lnk.to/ModalSoul All rights, including the master rights and ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/0qR3B1i3FBk/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/0qR3B1i3FBk/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/0qR3B1i3FBk/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "Nujabes",
+                "liveBroadcastContent": "none",
+                "publishTime": "2023-01-26T08:00:00Z"
+            }
+        }
+    ]
+}
